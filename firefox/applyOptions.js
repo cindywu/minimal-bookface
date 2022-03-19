56edbe07d18341da71f6326edc752774a5b0adeb
cindy <ciindy.wu@gmail.com>
2022-03-18 14:09:25 -1000

    firefox

---
+++ firefox/applyOptions.js
@@ -0,0 +1,66 @@
+function addStyles(css) {
+  var head = document.querySelector("head");
+  var style = document.createElement("style");
+  style.textContent = `${css}`;
+  head.appendChild(style);
+}
+
+chrome.storage.sync.get(
+  {
+    feedWidth: "600",
+    topNavigation: false,
+    noTweetButton: false,
+    showMessageDrawer: false,
+    feedBorders: false,
+    noBorders: false,
+    noLikes: false,
+    noRetweets: false,
+    noExploreButton: false,
+    noNotificationsButton: false,
+    noBookmarksButton: false,
+    noListsButton: false,
+    noDirectMessageButton: false,
+    noBanner: false,
+  },
+
+  function (items) {
+    if (items.topNavigation === true) {
+      addStyles(`
+      div[class="DashboardPage-module__welcome___2ChcI"],
+      div[class="DashboardPage-module__boxParent___2bwnw"] {
+        display: none;
+      }
+      `);
+    }
+
+    if (items.feedBorders === true) {
+      addStyles(`
+      div[class="post-list-item-comment"] {
+        display: none;
+      }
+      `);
+    }
+
+    if (items.noBorders === true) {
+      addStyles(`
+      div[class="recent-comments"] {
+        display: none;
+      }
+      `);
+    }
+
+    if (items.noBanner === true) {
+      addStyles(`
+      div[class="nag-div"] {
+        display: none;
+      }
+      `);
+    }
+  }
+);
+
+
+
+
+
+
---
+++ firefox/options.js
@@ -0,0 +1,46 @@
+// Saves options to chrome.storage
+function save_options() {
+  var topNavigation = document.getElementById("top-navigation").checked;
+  var feedBorders = document.getElementById("feed-borders").checked;
+  var noBorders = document.getElementById("borders").checked;
+  var noBanner = document.getElementById("no-banner").checked;
+
+  chrome.storage.sync.set(
+    {
+      topNavigation: topNavigation,
+      feedBorders: feedBorders,
+      noBorders: noBorders,
+      noBanner: noBanner,
+    },
+    function () {
+      // Update status to let user know options were saved.
+      var status = document.getElementById("status");
+      status.textContent = "Options saved.";
+      setTimeout(function () {
+        status.textContent = "";
+      }, 750);
+    }
+  );
+}
+
+// Restores select box and checkbox state using the preferences
+// stored in chrome.storage.
+function restore_options() {
+  chrome.storage.sync.get(
+    {
+      topNavigation: false,
+      feedBorders: false,
+      noBorders: false,
+      noBanner: false,
+    },
+    function (items) {
+      document.getElementById("top-navigation").checked = items.topNavigation;
+      document.getElementById("feed-borders").checked = items.feedBorders;
+      document.getElementById("borders").checked = items.noBorders;
+      document.getElementById("no-banner").checked = items.noBanner;
+
+    }
+  );
+}
+document.addEventListener("DOMContentLoaded", restore_options);
+document.getElementById("save").addEventListener("click", save_options);
